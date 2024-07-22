70e951e1b39c22fb129d85715ef8373d0f353453
DESKTOP-IMNSDE9\Administrator <kjuws9940@gmail.com>
2024-07-23 08:47:05 +0900

    멤버리파짓토리 추가

---
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package com.example.repository;
+
+import com.example.entity.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(Long id){
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findName(String name){
+        return em.createQuery("select m from Member m where m.memberName=:name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}

08707dbb9ddcc922f0ec229ba319fd250d77be24
yhood <kjuws9940@gmail.com>
2024-08-22 19:34:51 +0900

    로그인 세션처리

--- src/main/java/com/example/jpashop/controller/MvcTestController.java
+++ src/main/java/com/example/jpashop/controller/MvcTestController.java
@@ -1,6 +1,7 @@
 package com.example.jpashop.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,20 +9,69 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.Map;
 
 @RestController
 @Slf4j
 public class MvcTestController {
 
-    @PostMapping("/test")
-    public void test(@RequestBody Map<String, Object> paramMap, BindingResult bindingResult) {
-        log.info("test");
-        log.info("paramMap: {}", paramMap);
+    @PostMapping("/login")
+    public void login(@RequestBody Map<String, Object> paramMap , BindingResult bindingResult ,HttpServletRequest request) {
+        log.info("login");
         log.info("paramMap: {}", paramMap.get("id"));
         log.info("paramMap: {}", paramMap.get("pw"));
-        log.info("objectName={}", bindingResult.getObjectName());
-        log.info("target={}", bindingResult.getTarget());
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("에러발생");
+        }
+
+
+        HttpSession session = request.getSession();
+        session.setAttribute("id", paramMap.get("id"));
+        session.setMaxInactiveInterval(1800); //1800초
+    }
+
+    @PostMapping("/select")
+    public void select(HttpServletRequest request) {
+        log.info("select");
+
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            System.out.println("세션 없으면 가라");
+            return;
+        }
+        log.info("id:{}",session.getAttribute("id"));
+
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name -> log.info("session name={}, value={}",
+                        name, session.getAttribute(name)));
+        log.info("sessionId={}", session.getId());
+        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", new Date(session.getCreationTime()));
+        log.info("lastAccessedTime={}", new
+                Date(session.getLastAccessedTime()));
+        log.info("isNew={}", session.isNew());
+
+    }
+
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        log.info("logout");
+
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            System.out.println("세션 없으면 가라");
+            return;
+        }else{
+            System.out.println("세션 삭제");
+            session.invalidate();//세션 삭제
+        }
+        //에러발생
+        //log.info("id:{}",session.getAttribute("id"));
     }
 
 }
